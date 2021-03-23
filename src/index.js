e3f8b631f74fbb4bcdcebab7f1b6666e9695b722
Luc Patiny <luc@patiny.com>
2021-03-23 13:19:38 +0100

    feat: add getAnnotations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-export {
-  Analysis,
-  AnalysesManager,
-  fromJcamp,
-  toJcamp,
-  JSGraph,
-} from 'common-spectrum';
+import { JSGraph as OriginalJSGraph } from 'common-spectrum';
 
+import { getAnnotations } from './jsgraph/getAnnotations';
+
+export { Analysis, AnalysesManager, toJcamp } from 'common-spectrum';
+
+export { fromJcamp } from './from/fromJcamp';
+
+export const JSGraph = { ...OriginalJSGraph, getAnnotations };
