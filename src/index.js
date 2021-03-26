a950e2d7a4c02009f6b3c5c82f9f264193f25345
Luc Patiny <luc@patiny.com>
2021-03-26 12:14:35 +0100

    feat: add peakPicking and autoPeakPicking

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import { JSGraph as OriginalJSGraph } from 'common-spectrum';
 
 import { getAnnotations } from './jsgraph/getAnnotations';
 
-export { Analysis, AnalysesManager, toJcamp } from 'common-spectrum';
+export {
+  Analysis,
+  AnalysesManager,
+  toJcamp,
+  peakPicking,
+  autoPeakPicking,
+} from 'common-spectrum';
 
 export { fromJcamp } from './from/fromJcamp';
 
